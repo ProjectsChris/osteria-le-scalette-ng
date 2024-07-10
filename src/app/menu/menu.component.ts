c6e866a9ad33cf6c3664d30d8772022ebd0e7e5e
ProjectsChris <christopher2002buffon@gmail.com>
2024-07-10 17:47:55 +0200

    Fix styles

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ols-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
-
+export class MenuComponent implements OnInit {
+  ngOnInit(): void {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  }
 }
