ae101a69a52e4c276df87d98af2a425a33ed2b54
ProjectsChris <christopher2002buffon@gmail.com>
2024-07-14 18:04:28 +0200

    Removes files .sped.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  images: string[] = ["https://pipe-without.pockethost.io/api/files/dch2mql50yitdmc/1379hqhemzj7s2u/photo0jpg_cn6ZL86A9Q.jpg", "https://pipe-without.pockethost.io/api/files/8ivefog6sm5n1ku/sqy6bgrasazpal7/0ddcd76d_e98e_412a_bc39_3b0cbd1a8e73_azIoJfYnya.JPG"]
   constructor() {
   }
 }
